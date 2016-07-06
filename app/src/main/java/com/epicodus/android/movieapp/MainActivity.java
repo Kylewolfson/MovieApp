0adeeb2e757afbef949ac18274a6d4f5b6c7c593
Andrew Russo and Kyle Wolfson <andrew@>
2016-07-06 11:32:12 -0700

    Successfully created search API call that is able to pull in JSON data of movies based on title.

--- app/src/main/java/com/epicodus/android/movieapp/MainActivity.java
+++ app/src/main/java/com/epicodus/android/movieapp/MainActivity.java
@@ -1,13 +1,36 @@
 package com.epicodus.android.movieapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.searchButton) Button mSearchButton;
+    @Bind(R.id.searchParam) EditText mSearchParam;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mSearchButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mSearchButton) {
+            String searchParam = mSearchParam.getText().toString();
+            Intent intent = new Intent(MainActivity.this, SearchResultsActivity.class);
+            intent.putExtra("searchParam", searchParam);
+            startActivity(intent);
+        }
     }
 }
---
+++ app/src/main/java/com/epicodus/android/movieapp/SearchResultsActivity.java
@@ -0,0 +1,53 @@
+package com.epicodus.android.movieapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+public class SearchResultsActivity extends AppCompatActivity {
+    public static final String TAG = SearchResultsActivity.class.getSimpleName();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_results);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String searchParam = intent.getStringExtra("searchParam");
+        getMovies("query", searchParam);
+    }
+
+        private void getMovies(String searchType, String searchParam) {
+        final MovieService movieService = new MovieService();
+        movieService.findMovies(searchType, searchParam, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    String jsonData = response.body().string();
+                    Log.v(TAG, jsonData);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+    }
+}
