5364fec3cc6ab8b358c372d289c95d31a70b975d
Andrew Russo and Kyle Wolfson <kyle@>
2016-07-06 09:42:25 -0700

    Added paths for API key and set up OKHttp in the build.gradle.

---
+++ app/src/main/java/com/epicodus/android/movieapp/Constants.java
@@ -0,0 +1,8 @@
+package com.epicodus.android.movieapp;
+
+/**
+ * Created by Guest on 7/6/16.
+ */
+public class Constants {
+    public static final String MOVIE_DB_API_KEY = BuildConfig.MOVIE_DB_API_KEY;
+}
